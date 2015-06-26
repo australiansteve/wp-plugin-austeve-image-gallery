9a0c9199d30be5db3f4a663285bdf4ed80293ee7
australiansteve <steve@australiansteve.com>
2015-06-26 15:12:31 -0300

    Add admin settings page that has a setting for the number of sidebars to register. Still requires the theme to add a page template for each sidebar

---
+++ admin.php
@@ -0,0 +1,68 @@
+<?php // display the admin options page
+function austeve_image_gallery_options_page() {
+?>
+<div>
+	<h2>AUSteve Gallery settings</h2>
+	Options relating to the AUSteve Image Gallery plugin.
+
+	<form action="options.php" method="post">
+
+		<?php settings_fields('austeve_image_gallery_options'); ?>
+		<?php do_settings_sections('austeve_image_gallery'); ?>
+	 
+		<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+	</form>
+</div>
+ 
+<?php
+}?>
+
+<?php 
+function austeve_image_gallery_section_text() {
+	echo '<p></p>';
+} 
+?>
+
+<?php 
+function austeve_image_gallery_setting_num_sidebars() {
+	$options = get_option('austeve_image_gallery_options');
+	$num_sidebars_val = isset($options['num_sidebars']) ? $options['num_sidebars'] : '1';
+	echo "<input id='austeve_image_gallery_num_sidebars' name='austeve_image_gallery_options[num_sidebars]' size='40' type='text' value='{$num_sidebars_val}' />";
+} 
+?>
+
+<?php // add the admin settings and such
+function austeve_image_gallery_admin_init(){
+	register_setting( 'austeve_image_gallery_options', 'austeve_image_gallery_options', 'austeve_image_gallery_options_validate' );
+	add_settings_section('austeve_image_gallery_main', 'Main Settings', 'austeve_image_gallery_section_text', 'austeve_image_gallery');
+	add_settings_field('austeve_image_gallery_num_sidebars', 'Number of sidebars [1-9]:', 'austeve_image_gallery_setting_num_sidebars', 'austeve_image_gallery', 'austeve_image_gallery_main');
+}
+add_action('admin_init', 'austeve_image_gallery_admin_init');
+?>
+
+<?php // validate our options
+function austeve_image_gallery_options_validate($input) {
+	$options = get_option('austeve_image_gallery_options');
+	$options['num_sidebars'] = trim($input['num_sidebars']);
+
+	if(!preg_match('/^[1-9]$/i', $options['num_sidebars'])) {
+		$options['num_sidebars'] = '1';
+
+		add_settings_error(
+	        'sidebars_non_numeric',
+	        esc_attr( 'settings_updated' ),
+	        'Number of sidebars must be 1-9. Default of 1 used',
+	        'error'
+	    );
+	    return;
+	}
+	return $options;
+}
+?>
+
+<?php // add the admin options page itself
+function austeve_gallery_admin_add_page() {
+    add_options_page('AUSteve Gallery settings', 'AUSteve Gallery settings', 'manage_options', 'austeve_image_gallery', 'austeve_image_gallery_options_page');
+}
+add_action('admin_menu', 'austeve_gallery_admin_add_page');
+?>
--- austeve-image-gallery.php
+++ austeve-image-gallery.php
@@ -1,13 +1,16 @@
 <?php
 /**
- * Plugin Name: austeve Image Gallery
+ * Plugin Name: AUSteve Image Gallery
  * Plugin URI: https://github.com/australiansteve/wp-plugins/austeve-image-gallery
- * Description: Display a set on images in a page or post
- * Version: 1.0.3
+ * Description: Display a set of images in a page or post
+ * Version: 1.0.4
  * Author: AustralianSteve
  * Author URI: http://AustralianSteve.com
  * License: GPL2
  */
+
+include( plugin_dir_path( __FILE__ ) . 'admin.php');
+include( plugin_dir_path( __FILE__ ) . 'widget.php');
 
 function austeve_image_gallery_styles() {
     wp_enqueue_style( 'slick_styles', plugin_dir_url( __FILE__ ) . 'bower_components/slick.js/slick/slick.css', array() );
@@ -103,125 +106,4 @@ function add_init_scripts() {
 }
 add_action('init', 'add_init_scripts');
 
-
-// Creating the widget 
-class austeve_gallery_widget extends WP_Widget {
-
-    function __construct() {
-        parent::__construct(
-        // Base ID of your widget
-        'austeve_gallery_widget', 
-
-        // Widget name will appear in UI
-        __('AUSteve Gallery Widget', 'austeve_gallery_widget_domain'), 
-
-        // Widget description
-        array( 'description' => __( 'Preview for a gallery', 'austeve_gallery_widget_domain' ), ) 
-        );
-    }
-
-    // Creating widget front-end
-    // This is where the action happens
-    public function widget( $args, $instance ) {
-        $title = apply_filters( 'widget_title', $instance['title'] );
-        // before and after widget arguments are defined by themes
-        echo $args['before_widget'];
-
-        // This is where you run the code and display the output
-        $widgetOutput = "<div class='container'>";
-        $widgetOutput .= "<div class='layover'>";
-        $widgetOutput .= "<div class='header'><h2 class='title'>".$instance['title']."</h2></div>";
-        $widgetOutput .= "<div class='middle'><div class='description'>".$instance['description']."</div></div>";
-        
-        if (isset($instance['action_url'])) {
-            $widgetOutput .= "<div class='footer'><a href='".$instance['action_url']."' class='button' title='".$instance['title']."'>".$instance['action_verb']."</a></div>";
-        }
-        $widgetOutput .= "</div>"; //div.layover
-        $widgetOutput .= "<div class='img'><img src='".$instance['preview_image']."'/></div>";
-        $widgetOutput .= "</div>"; //div.container
-
-        echo __( $widgetOutput, 'austeve_gallery_widget_domain' );
-        echo $args['after_widget'];
-    }
-        
-    // Widget Backend 
-    public function form( $instance ) {
-        if ( isset( $instance[ 'title' ] ) ) {
-            $title = $instance[ 'title' ];
-        }
-        else {
-            $title = __( 'Gallery title', 'austeve_gallery_widget_domain' );
-        }
-
-        if ( isset( $instance[ 'preview_image' ] ) ) {
-            $preview_image = $instance[ 'preview_image' ];
-        }
-        else {
-            $preview_image = __( '%image url%', 'austeve_gallery_widget_domain' );
-        }
-        
-        if ( isset( $instance[ 'description' ] ) ) {
-            $description = $instance[ 'description' ];
-        }
-        else {
-            $description = __( '', 'austeve_gallery_widget_domain' );
-        }
-        
-        if ( isset( $instance[ 'action_url' ] ) ) {
-            $action_url = $instance[ 'action_url' ];
-        }
-        else {
-            $action_url = __( '', 'austeve_gallery_widget_domain' );
-        }
-        
-        if ( isset( $instance[ 'action_verb' ] ) ) {
-            $action_verb = $instance[ 'action_verb' ];
-        }
-        else {
-            $action_verb = __( 'Text to show on link button', 'austeve_gallery_widget_domain' );
-        }
-
-        // Widget admin form
-?>
-        <p>
-        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-        <label for="<?php echo $this->get_field_id( 'preview_image' ); ?>"><?php _e( 'Preview image:' ); ?></label> 
-        <input class="widefat" id="<?php echo $this->get_field_id( 'preview_image' ); ?>" name="<?php echo $this->get_field_name( 'preview_image' ); ?>" type="text" value="<?php echo esc_attr( $preview_image ); ?>" />
-        <label for="<?php echo $this->get_field_id( 'description' ); ?>"><?php _e( 'Short description:' ); ?></label> 
-        <input class="widefat" id="<?php echo $this->get_field_id( 'description' ); ?>" name="<?php echo $this->get_field_name( 'description' ); ?>" type="text" value="<?php echo esc_attr( $description ); ?>" />
-        <label for="<?php echo $this->get_field_id( 'action_url' ); ?>"><?php _e( 'Link URL:' ); ?></label> 
-        <input class="widefat" id="<?php echo $this->get_field_id( 'action_url' ); ?>" name="<?php echo $this->get_field_name( 'action_url' ); ?>" type="text" value="<?php echo esc_attr( $action_url ); ?>" />
-        <label for="<?php echo $this->get_field_id( 'action_verb' ); ?>"><?php _e( 'Link wording:' ); ?></label> 
-        <input class="widefat" id="<?php echo $this->get_field_id( 'action_verb' ); ?>" name="<?php echo $this->get_field_name( 'action_verb' ); ?>" type="text" value="<?php echo esc_attr( $action_verb ); ?>" />
-        </p>
-<?php 
-    }
-        
-    // Updating widget replacing old instances with new
-    public function update( $new_instance, $old_instance ) {
-        $instance = array();
-        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-        $instance['preview_image'] = ( ! empty( $new_instance['preview_image'] ) ) ? strip_tags( $new_instance['preview_image'] ) : '';
-        $instance['description'] = ( ! empty( $new_instance['description'] ) ) ? strip_tags( $new_instance['description'] ) : '';
-        $instance['action_url'] = ( ! empty( $new_instance['action_url'] ) ) ? strip_tags( $new_instance['action_url'] ) : '';
-        $instance['action_verb'] = ( ! empty( $new_instance['action_verb'] ) ) ? strip_tags( $new_instance['action_verb'] ) : 'Details';
-        return $instance;
-    }
-} // Class austeve_gallery_widget ends here
-
-// Register and load the widget
-function austeve_gallery_load_widget() {
-    register_widget( 'austeve_gallery_widget' );
-
-    register_sidebar( array(
-        'name'          => 'Gallery preview sidebar',
-        'id'            => 'austeve_gallery_1',
-        'before_widget' => '<li class="widget_austeve_gallery_widget">',
-        'after_widget'  => '</li>',
-        'before_title'  => '',
-        'after_title'   => '',
-    ) );
-}
-add_action( 'widgets_init', 'austeve_gallery_load_widget' );
 ?>
